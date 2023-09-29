74aee52df7b32b298378afc23f838650822642e2
riteshgaigawali <riteshgaigawali2001@gmail.com>
2023-09-29 16:03:35 +0530

    Added JavaClasses

---
+++ JavaClasses.java
@@ -0,0 +1,17 @@
+class Calculator {
+    int add(int a, int b) {
+        int result = a + b;
+        return result;
+    }
+
+}
+
+public class JavaClasses {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println("The addition is " + c.add(5, 6));
+
+        Calculator c1 = new Calculator();
+        System.out.println("The addition is " + c1.add(8, 6));
+    }
+}
