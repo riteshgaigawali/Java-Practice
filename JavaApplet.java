0ca77c209e70852982af2cc91cacdd4b55dc617b
riteshgaigawali <riteshgaigawali2001@gmail.com>
2024-10-17 22:13:08 +0530

    Added SuppressWarnings to JavaApplet

--- JavaApplet.java
+++ JavaApplet.java
@@ -1,6 +1,7 @@
 import java.applet.Applet;
 import java.awt.Graphics;
 
+@SuppressWarnings("deprecation")
 public class JavaApplet extends Applet {
     public void paint(Graphics g) {
         g.drawString("Hello, World!", 150, 150);
