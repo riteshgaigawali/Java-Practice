e58678788fca072facdfda06eca1273216ec468d
riteshgaigawali <riteshgaigawali2001@gmail.com>
2024-11-01 22:29:00 +0530

    Refactored

--- problems/FrequencyOfEle.java
+++ problems/FrequencyOfEle.java
@@ -9,13 +9,15 @@ public class FrequencyOfEle {
         int[] arr = { 8, 5, 6, 8, 4, 6, 9, 7, 5, 2, 1, 3 };
 
         for (int i = 0; i < arr.length; i++) {
-            int count = 0;
-            for (int j = 0; j < arr.length; j++) {
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] == arr[j]) {
                     count++;
+                    arr[j] = 0;
                 }
             }
-            System.out.println(arr[i] + " : " + count);
+            if (arr[i] != 0)
+                System.out.println(arr[i] + " : " + count);
         }
 
     }
